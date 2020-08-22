a7ca00f4873469468f6247dee70c8310bdf87f1f
lintooy <ianlindley.olmoguez@gmail.com>
2020-08-22 15:07:15 +0800

    Modified user login inputs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,13 +21,14 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Login</h1>
                                     </div>
-                                    <form method="POST" action="{{ route('login') }}">
+                                    <form class="user" method="POST" action="{{ route('login') }}">
                                         @csrf
 
                                         <div class="form-group">
                                             <input id="email" type="email"
-                                                class="form-control @error('email') is-invalid @enderror" name="email"
-                                                value="{{ old('email') }}" placeholder="Email Address" required autocomplete="email" autofocus>
+                                                class="form-control form-control-user @error('email') is-invalid @enderror"
+                                                name="email" value="{{ old('email') }}" placeholder="Email Address"
+                                                required autocomplete="email" autofocus>
 
                                             @error('email')
                                             <span class="invalid-feedback" role="alert">
@@ -36,21 +37,18 @@
                                             @enderror
                                         </div>
 
-                                        <div class="form-group row">
-                                            <label for="password"
-                                                class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                        <div class="form-group">
 
-                                            <div class="col-md-6">
-                                                <input id="password" type="password"
-                                                    class="form-control @error('password') is-invalid @enderror"
-                                                    name="password" required autocomplete="current-password">
+                                            <input id="password" type="password"
+                                                class="form-control form-control-user @error('password') is-invalid @enderror"
+                                                name="password" placeholder="Password" required
+                                                autocomplete="current-password">
 
-                                                @error('password')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                                @enderror
-                                            </div>
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
                                         </div>
 
                                         <div class="form-group row">
